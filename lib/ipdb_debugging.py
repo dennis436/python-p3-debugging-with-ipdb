3f2efc3ce4f7373188d0c4a20c128f8a83e43be2
dennis.muchiri1@student.moringaschool.com <dennis.muchiri1@student.moringaschool.com>
2025-05-19 10:32:29 +0300

    completed

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,11 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num=num + 2
+    return num
+import ipdb
+
+def plus_two(num):
+    num = num + 2
+    ipdb.set_trace()  # Program will pause here, let you inspect `num`
     return num
